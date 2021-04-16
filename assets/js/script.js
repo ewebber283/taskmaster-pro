b5b1474ccc3fdc0122ef0ebb568705c02a274023
Eric Webber <ewebber283@gmail.com>
2021-04-16 16:14:30 -0500

    added date picker and used moment

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,9 @@ var createTask = function(taskText, taskDate, taskList) {
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
+  // check due date
+  auditTask(taskLi);
+
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
 };
@@ -204,7 +207,14 @@ $(".list-group").on("click", "span", function() {
     .addClass("form-control")
     .val(date);
   $(this).replaceWith(dateInput);
-
+// enable JQ ui datepicker
+  dateInput.datepicker({ 
+    minDate: 1,   
+    onClose: function() {
+      //when calendar is closed, force a change event on the `dateInput`
+      $(this).trigger("change");
+    }
+  });
   // automatically bring up the calendar
   dateInput.trigger("focus");
 });
@@ -231,6 +241,8 @@ $(".list-group").on("change", "input[type='text']", function() {
     .addClass("badge badge-primary badge-pill")
     .text(date);
     $(this).replaceWith(taskSpan);
+  // Pass tastks <li> element into auditTask(to check new due date)
+  auditTask($(taskSpan).closest(".list-group-item"));
 });
 
 // remove all tasks
@@ -243,5 +255,28 @@ $("#remove-tasks").on("click", function() {
   saveTasks();
 });
 
+$("#modalDueDate").datepicker({
+  // how many days after the current date we want to limit kick in
+  minDate: 1
+});
+
+var auditTask = function(taskEl) {
+  // get date from task element
+  var date = $(taskEl).find("span").text().trim();
+  // ensure it worked
+  console.log(date);
+
+  // convert to moment object at 5PM
+  var time = moment(date, "L").set("hour", 17);
+  // remove anty old classes from element
+  $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+  // apply new task if task is near/over due date
+  if (moment().isAfter(time)) {
+    $(taskEl).addClass("list-group-item-danger");
+  }
+  else if (Math.abs(moment().diff(time, "days"))<= 2) {
+    $(taskEl).addClass("list-group-item-warning");
+  }
+};
 // load tasks for the first time
 loadTasks();
